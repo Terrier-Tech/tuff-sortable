d1879494071e4163ac7cad2c18eb34ed445b6dcf
Andy <ajselvig@gmail.com>
2023-08-30 20:53:27 -0500

    create a cursor indicating where the drop will happen

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,6 +2,7 @@ import Vecs, {Vec} from 'tuff-core/vec.ts'
 import SortablePlugin from "./sortable-plugin.ts"
 import {Logger} from "tuff-core/logging.ts"
 import Rects, {Rect} from "./rects.ts"
+import Html from 'tuff-core/html.ts'
 
 const log = new Logger("Handlers")
 
@@ -17,8 +18,9 @@ type DropTarget = {
 
 type FlexDirection = 'row' | 'column' | 'row-reverse' | 'column-reverse'
 
-const dropHighlightClass = 'tuff-sortable-drop-target'
 const dragHighlightClass = 'tuff-sortable-dragging'
+const dropCursorClass = 'tuff-sortable-drop-cursor'
+const cursorSize = 16
 
 export class DragHandler {
 
@@ -76,6 +78,10 @@ export class DragHandler {
 
     }
 
+    /**
+     * Find the closest drop target for the given translation of the drag target.
+     * @param diff
+     */
     findDropTarget(diff: Vec) {
         let minDistance = 9999999
         this.dropTarget = undefined
@@ -90,14 +96,13 @@ export class DragHandler {
             }
         }
 
-        this.clearHighlights()
+        this.clearCursor()
 
         if (this.dropTarget) { // this should always be true
             if (this.dropTarget.elem == this.target) {
                 log.info("The target is the closest, nothing to change")
             }
             else {
-                this.dropTarget.elem.classList.add(dropHighlightClass)
                 this.computeInsert(this.dropTarget, diffedRect)
                 log.info(`Insert at ${this.dropTarget.insertDirection} (index ${this.dropTarget.insertIndex})`)
             }
@@ -149,19 +154,60 @@ export class DragHandler {
                 }
             }
         }
+        this.addDropCursor(dropTarget)
     }
 
 
-    clearHighlights() {
-        this.container.querySelectorAll(`.${dropHighlightClass}`).forEach(elem => {
-            elem.classList.remove(dropHighlightClass)
+    clearCursor() {
+        this.container.querySelector(`.${dropCursorClass}`)?.remove()
+    }
+
+    addDropCursor(dropTarget: DropTarget) {
+        const rect = {
+            x: dropTarget.elem.offsetLeft,
+            y: dropTarget.elem.offsetTop,
+            width: dropTarget.elem.offsetWidth,
+            height: dropTarget.elem.offsetHeight
+        }
+        const box = {...rect}
+
+        switch (dropTarget.insertDirection) {
+            case 'left':
+                box.x -= cursorSize
+                box.width = cursorSize
+                break
+            case 'right':
+                box.x += rect.width
+                box.width = cursorSize
+                break
+            case 'top':
+                box.y -= cursorSize
+                box.height = cursorSize
+                break
+            case 'bottom':
+                box.y += rect.height
+                box.height = cursorSize
+                break
+        }
+
+        const cursor = Html.createElement('div', div => {
+            div.class(dropCursorClass)
+            div.css({
+                left: `${box.x}px`,
+                top: `${box.y}px`,
+                width: `${box.width}px`,
+                height: `${box.height}px`,
+                position: 'absolute',
+                zIndex: '1000'
+            })
         })
+        this.container.append(cursor)
     }
 
     dispose() {
         this.target.style.transform = ''
         this.target.classList.remove(dragHighlightClass)
-        this.clearHighlights()
+        this.clearCursor()
         window.removeEventListener('mousemove', this.onMouseMove)
         window.removeEventListener('mouseup', this.onMouseUp)
     }
